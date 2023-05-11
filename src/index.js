1b021ac02c5cd2d43678395477ec3b93a55edada
lokesh tailor <lokeshtailor14@gmail.com>
2023-05-11 12:11:59 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import Home from './Home';
+import Home from './ecommorce/Home'
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div id= 'id'>
+
+      <Home/>
+  
+  </div> 
+
+  
+);
